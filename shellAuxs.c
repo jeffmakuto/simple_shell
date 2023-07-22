00580a96a4fcf9e0e5120ba44864f4de9004f1f0
Clara-Moraa <moraacalaine@gmail.com>
2023-07-22 01:41:26 -0700

    simple_shell

--- shellAuxs.c
+++ shellAuxs.c
@@ -1,36 +1,69 @@
 #include "shell.h"
-
 /**
- * _strlen - function that calculates the length of a string.
- * @str: The input string.
- * Return: The length of the string (excluding the null terminator).
+ * _strlen - returns length of a string
+ * @s: string to get length of
+ *
+ * Return: length of @s
  */
-int _strlen(const char *str)
-{
-    int len = 0;
-    while (str[len] != '\0')
-        len++;
-    return len;
-}
 
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+	{
+		length++;
+	}
+
+	return (length);
+}
 /**
- * _strcat - function that concatenates two strings.
- * @dest: The destination buffer to hold the concatenated string.
- * @src: The source string to be appended to the destination.
- * Return: A pointer to the concatenated string (same as dest).
+ * _strcmp - function compares two strings.
+ * @s1: first string to compare.
+ * @s2: second string to compare.
+ *
+ * Return: If s1 is less than s2 (less than 0),
+ * If s1 equal to s2 (0), s1 is greater than s2 (greater than 0).
  */
-char *_strcat(char *dest, const char *src)
+int _strcmp(char *s1, char *s2)
 {
-    int dest_len = _strlen(dest);
-    int i;
-
-    for (i = 0; src[i] != '\0'; i++)
-    {
-        dest[dest_len + i] = src[i];
-    }
-
-    dest[dest_len + i] = '\0';
-
-    return dest;
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
 }
+/**
+ * _strspn - gets length of a prefix substring
+ *
+ * @s: segment where the bytes are gotten from
+ * @accept: source of bytes used in s
+ *
+ * Return: number of bytes in s consisting of only those
+ *         from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, x;
 
+	for (i = 0; *(s + i) != '\0'; i++)
+	{
+		x = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
+		{
+			if (*(s + i) == *(accept + j))
+			{
+				x = 0;
+				break;
+			}
+		}
+		if (x == 1)
+			break;
+	}
+	return (i);
+}
