4742a766a7679904d187453bafbe07e2a9ba4e3c
Clara-Moraa <moraacalaine@gmail.com>
2023-07-24 11:13:33 -0700

    simple shell

--- shellAuxs1.c
+++ shellAuxs1.c
@@ -56,3 +56,21 @@ char *_strdup(char *str)
 
 	return (new_s);
 }
+/**
+ * _memcpy - copies n bytes from src to dest memory area
+ *
+ * @n: bytes to be copied
+ * @src: memmory area to be copied from
+ * @dest: momory area copied to
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		*(dest + x) = *(src + x);
+
+	return (dest);
+}
