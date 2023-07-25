b29bc721f6baf1bb7d43188082f08f679f291976
Clara-Moraa <moraacalaine@gmail.com>
2023-07-25 01:36:34 -0700

    simple shell

--- shell.h
+++ shell.h
@@ -65,4 +65,5 @@ char *_strchr(char *s, char c);
 char *_strdup(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strstr(char *haystack, char *needle);
+char *_strtok(char *line, char *delim);
 #endif /* SHELL_H */
